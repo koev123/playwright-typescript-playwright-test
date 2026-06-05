dc6d70378811ff655522b3be68bc8965b39ec3ac
koev123 <songkoevabc@gmail.com>
2026-06-05 10:02:45 +0700

    fixed

--- tests/visualComparison/visualComparision.test.ts
+++ tests/visualComparison/visualComparision.test.ts
@@ -2,5 +2,8 @@ import test from '@lib/BaseTest';
 import { expect } from '@playwright/test';
 test(`Verify Elements Page.`, async ({ page, loginPage }) => {
     await loginPage.navigateToURL();
-    expect(await page.screenshot()).toMatchSnapshot('MainPage.png');
+    await loginPage.verifyLoginPageIsVisible();
+    expect(await page.screenshot({ animations: 'disabled' })).toMatchSnapshot('MainPage.png', {
+        maxDiffPixelRatio: 0.02,
+    });
 });
